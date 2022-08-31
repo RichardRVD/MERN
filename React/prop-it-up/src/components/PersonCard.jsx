a6d48461a65dd49ad7266391c0151d083d62f525
Richard Rodriguez-Van Dusen <richardRVD88@gmail.com>
2022-08-31 13:58:49 -0500

    added Putting It Together assignment features

--- React/prop-it-up/src/components/PersonCard.jsx
+++ React/prop-it-up/src/components/PersonCard.jsx
@@ -3,13 +3,17 @@ import React, { Component } from 'react'
 export default class PersonCard extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            age: this.props.age,
+        }
     }
   render() {
     return (
       <div>
           <h1>Name: {this.props.firstName} {this.props.lastName}</h1>
-          <h3>Age: {this.props.age}</h3>
+          <h3>Age: {this.state.age}</h3>
           <h3>Hair Color: {this.props.hairColor}</h3>
+          <button onClick={() => this.setState({age: this.state.age + 1})}>Birthday Button for {this.props.firstName}</button>
       </div>
     )
   }
